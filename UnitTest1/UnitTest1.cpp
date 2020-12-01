b44bad1fe7f25af4e6f74c27c938618818f6dc12
fakeplasticearth <62474537+fakeplasticearth@users.noreply.github.com>
2020-12-01 19:40:03 +0300

    Add files via upload

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -22,14 +22,8 @@ namespace UnitTest1
 			vector <int> test_array;
 			test_array = to1d(test_matrix_ex, m, n);
 			vector <int> true_array = { 1, 2, 3, 4, 5, 6 };
-			bool flag = 1;
-
-			if (test_array.size() != m * n)
-				flag = 0;
-			for (int i = 0; i < m * n; i++)
-				if (true_array[i] != test_array[i])
-					flag = 0;
-			Assert::IsTrue(flag);
+			
+			Assert::IsTrue(true_array == test_array);
 		}
 
 		TEST_METHOD(TestMethod2)
@@ -40,28 +34,7 @@ namespace UnitTest1
 			vector <vector<int> > true_matrix = { {1, 2, 3, 4, 5},{6, 7, 8, 9, 10} };
 			vector< vector<int> > test_matrix = to2d(test_array, m, n);
 
-			bool flag = 1;
-			
-			if (test_matrix.size() != m)
-				flag = 0;
-			if (flag) {
-				for (vector<vector<int> >::iterator it = test_matrix.begin(); it != test_matrix.end(); it++) {
-					if ((*it).size() != n) {
-						flag = 0;
-						break;
-					}
-				}
-			}
-			if (flag) {
-				for (int i = 0; i < m; i++) {
-					for (int j = 0;j < n;j++)
-						if (test_matrix[i][j] != true_matrix[i][j]) {
-							flag = 0;
-							break;
-						}
-				}
-			}
-			Assert::IsTrue(flag);
+			Assert::IsTrue(test_matrix == true_matrix);
 		}
 	};
 }
